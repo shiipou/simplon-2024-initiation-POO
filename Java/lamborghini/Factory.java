5675ff0cc357c419ec12cf64683a23272cb1427d
CADET Flavien <flavien.cadet@externe.maif.fr>
2024-07-23 13:09:31 +0200

    feat: add main example of usage

--- Java/lamborghini/Factory.java
+++ Java/lamborghini/Factory.java
@@ -1,5 +1,40 @@
 package lamborghini;
 
 public abstract class Factory {
-    
+    public abstract Vehicule creerVehicule(VehiculeChoice type);
+
+    public static void main(String[] args) {
+        Factory factory = new LamborghiniFactory();
+
+        Vehicule voiture = factory.creerVehicule(LamborghiniVehiculeChoice.URUS);
+
+        voiture.start();
+
+        if(voiture instanceof WithGearBox) {
+            WithGearBox voitureWithGearBox = (WithGearBox)voiture;
+            voitureWithGearBox.setGear(1);
+        }
+
+        voiture.move();
+
+        voiture.stop();
+
+        Vehicule tracteur = factory.creerVehicule(LamborghiniVehiculeChoice.CRONO);
+
+        tracteur.start();
+
+        if(tracteur instanceof WithGearBox) {
+            WithGearBox tracteurWithGearBox = (WithGearBox)tracteur;
+            tracteurWithGearBox.setGear(1);
+        }
+        if(tracteur instanceof WithAttlage) {
+            WithAttlage tracteurWithAttlage = (WithAttlage)tracteur;
+            tracteurWithAttlage.attacheRemorque(new Remorque());
+        }
+
+        tracteur.move();
+
+        tracteur.stop();
+
+    }
 }
